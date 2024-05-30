0e3b9d3860c2c62dcd5400c2aee7d3eb9e4f2a5d
Aaron Coyne <acoyne40@stuy.edu>
2024-05-30 15:04:43 -0400

    Worked on encode

--- Util.java
+++ Util.java
@@ -1,3 +1,21 @@
 public class Util{
     //
+    public static void main(String[] args){
+        int output = 0;
+        int[] digits = digitEncode(new int[]{});
+        for(int i = 0; i < input.length; i++){  
+            
+        }
+    }
+    public static int[] digitEncode(int[] input, int[] key){
+        int[] output = new int[input.length];
+        int pos = 0;
+        int[] ident = new int[]{1,2,3,4,5,6,7,8,9,0};
+        for(int i = 0; i < input.length; i++){
+            if(input[i] == 0) output[i] = key[9];
+            else output[i] = key[(input[i]- 1)];
+        }
+        return output;
+        
+    }
 }
