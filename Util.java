59e10b29245efa30b62df8006f02617152209705
Riasat Audhy <riasat2audhy@gmail.com>
2024-05-30 14:46:46 -0400

    working on digit by digit modular addition & subtraction

--- Util.java
+++ Util.java
@@ -1,3 +1,16 @@
 public class Util{
-    //
+    public static int modAddition(int a, int b){
+        int sum = 0;
+
+        int numDigits = 0;
+        if (a > b) numDigits = a;
+        elese numDigits = b;
+        numDigits = String.valueOf(numDigits).length();
+
+        for(int i = 0; i < numDigits; i++){
+            //
+        }
+
+        return 0;
+    }
 }
