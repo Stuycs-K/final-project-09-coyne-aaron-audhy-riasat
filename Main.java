4dcf849e657494d49248c9383a22f02be8700fa9
Riasat Audhy <riasat2audhy@gmail.com>
2024-06-03 14:30:33 -0400

    digit by digit mod subtraction works now too!

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
 public class Main{
     public static void main(String[] args){
-        System.out.println(Util.modAddition(1234,6789));
+        System.out.println(Util.modSubtraction(1234,6789));
     }
 }
--- Util.java
+++ Util.java
@@ -13,4 +13,19 @@ public class Util{
 
         return sum;
     }
+    public static int modSubtraction(int a, int b){
+        int diff = 0;
+
+        int numDigits = 0;
+        if (a > b) numDigits = a;
+        else numDigits = b;
+        numDigits = String.valueOf(numDigits).length();
+
+        for(int i = 0; i < numDigits; i++){
+            if(a%Math.pow(10,i+1) - a%Math.pow(10,i) >= b%Math.pow(10,i+1) - b%Math.pow(10,i)) diff += (a%Math.pow(10,i+1) - a%Math.pow(10,i)) - (b%Math.pow(10,i+1) - b%Math.pow(10,i));
+            else diff += (a%Math.pow(10,i+1) - a%Math.pow(10,i) + Math.pow(10,i+1)) - (b%Math.pow(10,i+1) - b%Math.pow(10,i));
+        }
+
+        return diff;
+    }
 }
