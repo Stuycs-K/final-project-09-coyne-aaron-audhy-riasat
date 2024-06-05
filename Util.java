241133a895c7358191371f04f3367753b6e80563
Aaron Coyne <acoyne40@stuy.edu>
2024-06-05 16:18:26 -0400

    Fixed issues with sequenceLetters and completely finished sequenceNumbers

--- Util.java
+++ Util.java
@@ -4,10 +4,11 @@ public class Util{
         //int[] digits = digitEncode(new int[]{9,0,2,1,0}, new int[]{6,0,1,3,5,8,9,4,2,7});
         
         System.out.println(arrayToString(sequenceLetters("OCTOPUS")));
+        System.out.println(arrayToString(sequenceNumbers(new int[]{9,0,2,1,0})));
     }
     public static int[] digitEncode(int[] input, int[] key){
         int[] output = new int[input.length];
-        int pos = 0;
+        //int pos = 0;
         //int[] ident = new int[]{1,2,3,4,5,6,7,8,9,0};
         for(int i = 0; i < input.length; i++){
             if(input[i] == 0) output[i] = key[9];
@@ -18,56 +19,79 @@ public class Util{
     }
 
     public static int[] sequenceNumbers (int[] input){
-        return null;
+        int[] output = new int[input.length];
+        int[] tempOld = new int[input.length];
+        for(int i = 0; i < input.length; i++){
+            if(input[i] == 0) input[i] = 10;
+            tempOld[i] = input[i];
+            
+        }
+            int[] tempNew = bubbleSort(input);
+        int[] numbers = new int[]{1,2,3,4,5,6,7,8,9,0};
+        boolean[] isUsed = new boolean[input.length];
+        for(int i = 0; i < isUsed.length; i++){
+            isUsed[i] = false;
+        }
+        //FOR EACH VALUE IN SORTED, CHECK FOR MATCH IN UNSORTED
+        //SET output[Pos in Old] TO POSITION OF NEW
+        for(int i = 0; i < tempNew.length; i++){ 
+            for(int j = 0; j < tempOld.length; j++){
+                if(tempOld[j] == tempNew[i] && isUsed[j] == false){
+                    isUsed[j] = true;
+                    output[j] = numbers[i];
+                    j = tempOld.length;
+                }
+            }
+        }
+        return output;
+    }
         
 
-    }
+    
 
     public static int[] sequenceLetters (String input){
         int[] temp = new int[input.length()];
         int[] output = new int[input.length()];
+        //REPRESENT CHARACTERS AS ASCII VALUES AND SORT THEM
         for(int i = 0; i < input.length(); i++){
             temp[i] = (int)(input.charAt(i));
-           // System.out.println((int)(input.charAt(i)));
         }
         int[] tempOld = new int[temp.length];
         for(int i = 0; i < temp.length; i++)
             tempOld[i] = temp[i];
-        int[] tempNew = insertionSort(temp);
-        //System.out.println(arrayToString(tempOld));
-        //System.out.println(arrayToString(tempNew));
+        int[] tempNew = bubbleSort(temp);
         int[] numbers = new int[]{1,2,3,4,5,6,7,8,9,0};
         boolean[] isUsed = new boolean[temp.length];
         for(int i = 0; i < isUsed.length; i++){
             isUsed[i] = false;
         }
-        
-        for(int i = 0; i < tempNew.length; i++){ //UNFINISHED
+        //FOR EACH VALUE IN SORTED, CHECK FOR MATCH IN UNSORTED
+        //SET output[Pos in Old] TO POSITION OF NEW
+        for(int i = 0; i < tempNew.length; i++){ 
             for(int j = 0; j < tempOld.length; j++){
                 if(tempOld[j] == tempNew[i] && isUsed[j] == false){
                     isUsed[j] = true;
                     output[j] = numbers[i];
-
+                    j = tempOld.length;
                 }
             }
         }
         return output;
     }
 
-    public static int[] insertionSort(int[] input){
-		int key = 0;
-		for(int i = 0; i < (input.length - 1); i++){
-			key = input[i + 1];
-			for(int j = i; j > 0; j--){
-				if(input[j] < key)
-					input[j+1] = key;
-				else
-					input[j+1] = input[j];
+    public static int[] bubbleSort(int[] input){
+		int end = input.length - 1;
+		for(int i = 0; i < input.length; i++){
+			for(int j = 0; j < end; j++){
+				if(input[j] > input[j + 1]){
+					//swap
+					int swapHelper = input[j];
+					input[j] = input[j + 1];
+					input[j + 1] = swapHelper;
+				}
 					
 			}
-			if(key < input[i])
-				input[i] = key;
-				
+			end--;
 		}
 		return input;
 	}
