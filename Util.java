2480a854e12eb9c20f43e695804fe9cde9b09102
Aaron Coyne <acoyne40@stuy.edu>
2024-06-05 15:02:39 -0400

    Worked on Sequencing

--- Util.java
+++ Util.java
@@ -1,12 +1,9 @@
 public class Util{
     //
     public static void main(String[] args){
-        String output = "";
         int[] digits = digitEncode(new int[]{9,0,2,1,0}, new int[]{6,0,1,3,5,8,9,4,2,7});
-        for(int i = 0; i < digits.length; i++){  
-            output += digits[i] + " ";
-        }
-        System.out.println(output);
+        
+        System.out.println(arrayToString(sequenceLetters(EDFLO)));
     }
     public static int[] digitEncode(int[] input, int[] key){
         int[] output = new int[input.length];
@@ -20,8 +17,57 @@ public class Util{
         
     }
 
-    public static int[] sequence (String input){
+    public static int[] sequenceNumbers (int[] input){
+        return null;
         
+
+    }
+
+    public static int[] sequenceLetters (String input){
+        int[] temp = new int[input.length()];
+        int[] output = new int[input.length()]
+        for(int i = 0; i < input.length(); i++){
+            temp[i] = (int)((char)(input.Substring(i,i+1)));
+        }
+        int[] tempNew = insertionSort[temp];
+        
+        int[] numbers = new int[]{1,2,3,4,5,6,7,8,9,0};
+        boolean[] isUsed = new boolean[temp.length];
+        for(int i = 0; i < isUsed.length; i++){
+            isUsed[i] = false;
+        }
+        
+        for(int i = 0; i < tempNew.length; i++){ //UNFINISHED
+            if(temp[i] == tempNew[i] && isUsed[i] == false){
+                isUsed[i]
+            }
+        }
+    }
+
+    public static int[] insertionSort(int[] input){
+		int key = 0;
+		for(int i = 0; i < (input.length - 1); i++){
+			key = input[i + 1];
+			for(int j = i; j > 0; j--){
+				if(input[j] < key)
+					input[j+1] = key;
+				else
+					input[j+1] = input[j];
+					
+			}
+			if(key < input[i])
+				input[i] = key;
+				
+		}
+		return input;
+	}
+
+    public static String arrayToString(int[] input){
+        String output = "";
+        for(int i = 0; i < input.length; i++){  
+            output += input[i] + " ";
+        }
+        return output;
 
     }
 }
