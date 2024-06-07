26659d6e6540fdbe891a1d62d4eeeff53d2f13ba
Riasat Audhy <riasat2audhy@gmail.com>
2024-06-07 08:51:04 -0400

    modular subtraction converted to int[]

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 public class Main{
     public static void main(String[] args){
-        int a[] = {7,3,4};
-        int b[] = {5,8,4,5,6};
+        int a[] = {1,2,3,4};
+        int b[] = {6,7,8,9};
         int sum[] = Util.modAddition(a, b);
 
         for(int i = 0; i < sum.length; i++) System.out.print(sum[i] + ",");
--- Util.java
+++ Util.java
@@ -24,13 +24,43 @@ public class Util{
         int sum[] = new int[numDigits];
 
         for(int i = 0; i < numDigits; i++){
-            System.out.println(a[i] + b[i]);
             sum[i] = (a[i] + b[i])%10;
         }
 
         return sum;
     }
 
+    public static int[] modSubtraction(int[] a, int[] b){
+        int numDigits = 0;
+        int temp[];
+        if (a.length > b.length){ 
+            numDigits = a.length;
+            temp = new int[numDigits];
+            for(int i = 0; i < numDigits; i++){
+                if(i < a.length-b.length) temp[i] = 0;
+                else temp[i] = b[i - (b.length-a.length)];
+            }
+            b = temp;
+        }
+        else {
+            numDigits = b.length;
+            temp = new int[numDigits];
+            for(int i = 0; i < numDigits; i++){
+                if(i < b.length-a.length) temp[i] = 0;
+                else temp[i] = a[i - (b.length-a.length)];
+            }
+            a = temp;
+        }
+
+        int diff[] = new int[numDigits];
+
+        for(int i = 0; i < numDigits; i++){
+            if (a[i] >= b[i]) diff[i] = a[i] - b[i];
+            else diff[i] = a[i] + 10 - b[i];
+        }
+
+        return diff;
+    }
     // public static int modSubtraction(int a, int b){
     //     int diff = 0;
 
